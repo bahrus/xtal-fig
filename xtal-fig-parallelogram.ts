e1db48ea997b98d3b21ac83da5e80518e1242535
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-07-23 19:26:01 -0400

    ...

--- xtal-fig-parallelogram.ts
+++ xtal-fig-parallelogram.ts
@@ -3,40 +3,20 @@ import {CCProps} from 'carbon-copy/types.d.js';
 import {html} from 'xtal-element/lib/html.js';
 
 const mainTemplate = html`
-<div id=parallelogram part=outer>
-    <div id=inner part=inner>
-        <slot></slot>
-    </div>
-</div>
-<style>
-:host{
-    --outer-background-color: lightblue;
-    --outer-skew:skew(20deg);
-    --inner-unskew:skew(-20deg);
-}
-</style>
-<style>
-:host{
-    display:block;
-}
-#parallelogram {
-   width: 100%; 
-   height: 100%;
-   background-color: var(--outer-background-color);
-   /* Skew */
-   -webkit-transform: var(--outer-skew);
-   -moz-transform: var(--outer-skew); 
-   -o-transform: var(--outer-skew);
-   transform: var(--outer-skew);
-}
-#inner {
-    /* Unskew */
-    -webkit-transform: var(--inner-unskew); 
-   -moz-transform: var(--inner-unskew); 
-   -o-transform: var(--inner-unskew);
-   transform: var(--inner-unskew);
-}
-</style>
+<svg xmlns="http://www.w3.org/2000/svg" width="800" height="300">
+    <path d="M 205,5 L 795,5 L 595,295 L 5,295 L 205,5 z" id="path59" style="fill:#ccff00;stroke:none" />
+    <path d="M 205,5 L 795,5 L 595,295 L 5,295 L 205,5 z" id="path61" style="fill:none;stroke:#000000;stroke-width:5;stroke-linejoin:round" />
+    <g>
+        <foreignobject x=100 width=100 height=500 requiredExtensions="http://www.w3.org/1999/xhtml">
+                <!-- XHTML content goes here -->
+            <body xmlns="http://www.w3.org/1999/xhtml">
+                <p>Here is a paragraph that requires word wrap</p>
+            </body>
+        </foreignobject>
+    </g>
+    
+
+</svg>
 `;
 /**
  * @element xtal-fig-parallelogram
