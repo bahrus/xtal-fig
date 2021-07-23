78f752d1c828cc608bfa364a335ac42a1df1dfe7
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-07-23 19:56:03 -0400

    ...

--- xtal-fig-parallelogram.js
+++ xtal-fig-parallelogram.js
@@ -2,8 +2,8 @@ import { define } from 'carbon-copy/c-c.js';
 import { html } from 'xtal-element/lib/html.js';
 const mainTemplate = html `
 <svg xmlns="http://www.w3.org/2000/svg" width="800" height="300">
-    <path d="M {{topLeft}},5 L 795,5 L 595,295 L 5,295 L {{topLeft}},5 z" id="path59" style="fill:#ccff00;stroke:none" />
-    <path d="M {{topLeft}},5 L 795,5 L 595,295 L 5,295 L {{topLeft}},5 z" id="path61" style="fill:none;stroke:#000000;stroke-width:5;stroke-linejoin:round" />
+    <path d="M {{topLeft ?? 205}},5 L {{topRight ?? 795}},5 L {{bottomRight ?? 595}},295 L 5,295 L {{topLeft ?? 205}},5 z" id="path59" style="fill:#ccff00;stroke:none" />
+    <path d="M {{topLeft ?? 205}},5 L {{topRight ?? 795}},5 L {{bottomRight ?? 595}},295 L 5,295 L {{topLeft ?? 205}},5 z" id="path61" style="fill:none;stroke:#000000;stroke-width:5;stroke-linejoin:round" />
     <g>
         <foreignobject x=100 width=100 height=500 requiredExtensions="http://www.w3.org/1999/xhtml">
                 <!-- XHTML content goes here -->
@@ -21,5 +21,5 @@ const mainTemplate = html `
  * @tag xtal-fig-parallelogram
  */
 define('xtal-fig-parallelogram', mainTemplate, {
-    numProps: ['topLeft=205']
+    numProps: ['topLeft=205', 'topRight=795', 'bottomRight=595']
 });
