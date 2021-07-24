928ff693a7597538fadc622b1394548be737c766
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-07-24 10:23:13 -0400

    ...

--- xtal-fig-parallelogram.js
+++ xtal-fig-parallelogram.js
@@ -1,9 +1,17 @@
 import { define } from 'carbon-copy/c-c.js';
 import { html } from 'xtal-element/lib/html.js';
 const mainTemplate = html `
-<svg xmlns="http://www.w3.org/2000/svg" width="800" height="300">
+<style>
+    :host[hidden]{
+        display:none;
+    }
+    :host{
+        display:block;
+    } 
+</style>
+<svg xmlns="http://www.w3.org/2000/svg" width={{width}} height={{height}}">
     <path d="M {{topLeft ?? 205}},5 L {{topRight}},5 L {{bottomRight ?? 595}},295 L 5,295 L {{topLeft ?? 205}},5 z" id="path59" style="fill:#ccff00;stroke:none" />
-    <path d="M {{topLeft ?? 205}},5 L {{topRight}},5 L {{bottomRight ?? 595}},295 L 5,295 L {{topLeft ?? 205}},5 z" id="path61" style="fill:none;stroke:#000000;stroke-width:5;stroke-linejoin:round" />
+    <path d="M {{topLeft ?? 205}},5 L {{topRight}},5 L {{bottomRight ?? 595}},295 L 5,295 L {{topLeft ?? 205}},5 z" id="path61" style="fill:none;stroke:#000000;stroke-width:{{strokeWidth}};stroke-linejoin:round" />
     <g>
         <foreignobject x=100 width=100 height=500 requiredExtensions="http://www.w3.org/1999/xhtml">
                 <!-- XHTML content goes here -->
@@ -21,10 +29,14 @@ const mainTemplate = html `
  * @tag xtal-fig-parallelogram
  */
 define('xtal-fig-parallelogram', mainTemplate, {
-    numProps: ['topLeft=205', 'topRight', 'bottomRight=595', 'width=800'],
+    numProps: ['topLeft=205', 'topRight', 'bottomRight=595', 'width=800', 'strokeWidth=5', 'height=300'],
     propActionsProp: [
-        ({ width, self }) => {
-            self.topRight = width - 5;
+        ({ width, strokeWidth, self }) => {
+            self.topRight = width - strokeWidth;
+            self.style.width = width + 'px';
+        },
+        ({ height, self }) => {
+            self.style.height = height + 'px';
         }
     ]
 });
--- xtal-fig-parallelogram.ts
+++ xtal-fig-parallelogram.ts
@@ -3,9 +3,17 @@ import {CCProps} from 'carbon-copy/types.d.js';
 import {html} from 'xtal-element/lib/html.js';
 
 const mainTemplate = html`
-<svg xmlns="http://www.w3.org/2000/svg" width="800" height="300">
+<style>
+    :host[hidden]{
+        display:none;
+    }
+    :host{
+        display:block;
+    } 
+</style>
+<svg xmlns="http://www.w3.org/2000/svg" width={{width}} height={{height}}">
     <path d="M {{topLeft ?? 205}},5 L {{topRight}},5 L {{bottomRight ?? 595}},295 L 5,295 L {{topLeft ?? 205}},5 z" id="path59" style="fill:#ccff00;stroke:none" />
-    <path d="M {{topLeft ?? 205}},5 L {{topRight}},5 L {{bottomRight ?? 595}},295 L 5,295 L {{topLeft ?? 205}},5 z" id="path61" style="fill:none;stroke:#000000;stroke-width:5;stroke-linejoin:round" />
+    <path d="M {{topLeft ?? 205}},5 L {{topRight}},5 L {{bottomRight ?? 595}},295 L 5,295 L {{topLeft ?? 205}},5 z" id="path61" style="fill:none;stroke:#000000;stroke-width:{{strokeWidth}};stroke-linejoin:round" />
     <g>
         <foreignobject x=100 width=100 height=500 requiredExtensions="http://www.w3.org/1999/xhtml">
                 <!-- XHTML content goes here -->
@@ -23,10 +31,14 @@ const mainTemplate = html`
  * @tag xtal-fig-parallelogram
  */
 define('xtal-fig-parallelogram', mainTemplate, {
-    numProps:['topLeft=205', 'topRight', 'bottomRight=595', 'width=800'],
+    numProps:['topLeft=205', 'topRight', 'bottomRight=595', 'width=800', 'strokeWidth=5', 'height=300'],
     propActionsProp:[
-        ({width, self}: any) => {
-            self.topRight = width - 5;
+        ({width, strokeWidth, self}: any) => {
+            self.topRight = width - strokeWidth;
+            self.style.width = width + 'px';
+        },
+        ({height, self}: any) => {
+            self.style.height = height + 'px';
         }
     ]
 } as CCProps);
