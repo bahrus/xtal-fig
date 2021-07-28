b1535f194ee70a785d8a9c707f919b8b6b504846
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-07-27 21:39:46 -0400

    ...

--- xtal-fig-diamond.js
+++ xtal-fig-diamond.js
@@ -1,5 +1,7 @@
-import { define } from 'carbon-copy/c-c.js';
+import { xc } from 'xtal-element/lib/XtalCore.js';
+import { xp } from 'xtal-element/lib/XtalPattern.js';
 import { html } from 'xtal-element/lib/html.js';
+import { DOMKeyPEA } from 'xtal-element/lib/DOMKeyPEA.js';
 const mainTemplate = html `
 <style>
     :host[hidden]{
@@ -8,40 +10,78 @@ const mainTemplate = html `
     :host{
         display:block;
     } 
-
 </style>
 <svg xmlns="http://www.w3.org/2000/svg" width={{width}} height={{height}}>
     <path part=diamond-fill 
-        d={{path}} 
         style="fill:#ccff00;stroke:none" />
     <path part=diamond-border 
-        d={{path}} 
-        style="fill:none;stroke:#000000;stroke-width:{{strokeWidth}};stroke-linejoin:round;" />
+        style="fill:none;stroke:#000000;stroke-linejoin:round;" />
     <g>
-        <foreignObject part=inner width="{{innerWidth}}" height="{{innerHeight}}" x="{{innerX}}" y="{{innerY}}">
+        <foreignObject part=inner>
             <slot></slot>
         </foreignObject>
     </g>
-    
-
 </svg>
 `;
-/**
- * @element xtal-fig-diamond
- * @tag xtal-fig-diamond
- */
-define('xtal-fig-diamond', mainTemplate, {
-    numProps: ['width=800', 'height=300', 'innerWidth=200', 'strokeWidth=5', 'innerHeight=100', 'innerX=300', 'innerY=100'],
-    stringProps: ['path'],
-    propActionsProp: [
-        ({ height, self }) => {
-            self.style.height = height + 'px';
-        },
-        ({ width, self }) => {
-            self.style.width = width + 'px';
-        },
-        ({ height, width, strokeWidth, self }) => {
-            self.path = `M ${width / 2},${strokeWidth} L ${strokeWidth},${height / 2} L ${width / 2},${height - strokeWidth} L ${width - strokeWidth},${height / 2} L ${width / 2},${strokeWidth} z`;
-        },
-    ],
-});
+const refs = { svgElement: '', pathElements: '', innerPart: '', diamondBorderPart: '' };
+export class XtalFigDiamond extends HTMLElement {
+    static is = 'xtal-fig-diamond';
+    self = this;
+    propActions = propActions;
+    refs = refs;
+    mainTemplate = mainTemplate;
+    reactor = new xp.RxSuppl(this, [{
+            rhsType: Array,
+            ctor: DOMKeyPEA,
+        }]);
+    connectedCallback() {
+        xc.mergeProps(this, slicedPropDefs, {
+            width: 800, height: 300, innerWidth: 200, strokeWidth: 5, innerHeight: 100, innerX: 300, innerY: 100
+        });
+    }
+    onPropChange(n, prop, nv) {
+        this.reactor.addToQueue(prop, nv);
+    }
+}
+const propActions = [
+    xp.manageMainTemplate,
+    ({ width, height }) => [{
+            [refs.svgElement]: [, , { width: width, height: height }]
+        }],
+    ({ domCache, width, strokeWidth, height }) => [{
+            [refs.pathElements]: [, , { d: `M ${width / 2},${strokeWidth} L ${strokeWidth},${height / 2} L ${width / 2},${height - strokeWidth} L ${width - strokeWidth},${height / 2} L ${width / 2},${strokeWidth} z`, }],
+        }],
+    ({ domCache, strokeWidth }) => [{
+            [refs.diamondBorderPart]: [{ style: { strokeWidth: strokeWidth } }],
+        }],
+    ({ domCache, innerHeight, innerWidth, innerX, innerY }) => [{
+            [refs.innerPart]: [, , { width: innerWidth, height: innerHeight, x: innerX, y: innerY }],
+        }],
+    xp.createShadow,
+];
+const baseProp = {
+    dry: true,
+    async: true,
+};
+const numProp = {
+    ...baseProp,
+    type: Number,
+};
+const strProp = {
+    ...baseProp,
+    type: String,
+};
+const propDefMap = {
+    ...xp.props,
+    width: numProp,
+    height: numProp,
+    innerWidth: numProp,
+    strokeWidth: numProp,
+    innerHeight: numProp,
+    innerX: numProp,
+    innerY: numProp,
+    path: strProp,
+};
+const slicedPropDefs = xc.getSlicedPropDefs(propDefMap);
+xc.letThereBeProps(XtalFigDiamond, slicedPropDefs, 'onPropChange');
+xc.define(XtalFigDiamond);
--- xtal-fig-diamond.ts
+++ xtal-fig-diamond.ts
@@ -1,7 +1,9 @@
-import {define} from 'carbon-copy/c-c.js';
-import {CCProps} from 'carbon-copy/types.d.js';
+import {xc, IReactor, PropAction, PropDef, PropDefMap, ReactiveSurface} from 'xtal-element/lib/XtalCore.js';
+import {xp, XtalPattern} from 'xtal-element/lib/XtalPattern.js';
 import {html} from 'xtal-element/lib/html.js';
-import {XtalFigDiamondProps} from './types.d.js';
+import {XtalFigDiamondProps} from './types.js';
+import {DOMKeyPEA} from 'xtal-element/lib/DOMKeyPEA.js';
+
 const mainTemplate = html`
 <style>
     :host[hidden]{
@@ -10,41 +12,80 @@ const mainTemplate = html`
     :host{
         display:block;
     } 
-
 </style>
 <svg xmlns="http://www.w3.org/2000/svg" width={{width}} height={{height}}>
     <path part=diamond-fill 
-        d={{path}} 
         style="fill:#ccff00;stroke:none" />
     <path part=diamond-border 
-        d={{path}} 
-        style="fill:none;stroke:#000000;stroke-width:{{strokeWidth}};stroke-linejoin:round;" />
+        style="fill:none;stroke:#000000;stroke-linejoin:round;" />
     <g>
-        <foreignObject part=inner width="{{innerWidth}}" height="{{innerHeight}}" x="{{innerX}}" y="{{innerY}}">
+        <foreignObject part=inner>
             <slot></slot>
         </foreignObject>
     </g>
-    
-
 </svg>
 `;
-type X = XtalFigDiamondProps;
-/**
- * @element xtal-fig-diamond
- * @tag xtal-fig-diamond
- */
-define('xtal-fig-diamond', mainTemplate, {
-    numProps: ['width=800', 'height=300', 'innerWidth=200', 'strokeWidth=5', 'innerHeight=100', 'innerX=300', 'innerY=100'],
-    stringProps:['path'],
-    propActionsProp: [
-        ({height, self}: X) => {
-            self.style.height = height + 'px';
-        },
-        ({width, self}: X) => {
-            self.style.width = width + 'px';
-        },
-        ({height, width, strokeWidth, self}: X) => {
-            self.path = `M ${width / 2},${strokeWidth} L ${strokeWidth},${height / 2} L ${width / 2},${height-strokeWidth} L ${width - strokeWidth},${height / 2} L ${width / 2},${strokeWidth} z`;
-        },
-    ],
-} as CCProps);
+const refs = {svgElement: '', pathElements: '', innerPart: '', diamondBorderPart: ''};
+export class XtalFigDiamond extends HTMLElement implements ReactiveSurface, XtalPattern{
+    static is = 'xtal-fig-diamond';
+    self = this;
+    propActions = propActions;
+    refs = refs;
+    mainTemplate = mainTemplate;
+    reactor: IReactor = new xp.RxSuppl(this, [{
+        rhsType: Array,
+        ctor: DOMKeyPEA,
+    }]);
+    connectedCallback(){
+        xc.mergeProps<Partial<XtalFigDiamondProps>>(this, slicedPropDefs, {
+            width:800, height:300, innerWidth:200, strokeWidth:5, innerHeight:100, innerX:300, innerY:100
+        });
+    }
+    onPropChange(n: string, prop: PropDef, nv: any){
+        this.reactor.addToQueue(prop, nv);
+    }
+}
+export interface XtalFigDiamond extends XtalFigDiamondProps{}
+type X = XtalFigDiamond;
+const propActions = [
+    xp.manageMainTemplate,
+    ({width, height}: X) => [{
+        [refs.svgElement]: [,,{width:width, height: height}]
+    }],
+    ({domCache, width, strokeWidth, height}: X) => [{
+        [refs.pathElements]:  [,, {d: `M ${width / 2},${strokeWidth} L ${strokeWidth},${height / 2} L ${width / 2},${height-strokeWidth} L ${width - strokeWidth},${height / 2} L ${width / 2},${strokeWidth} z`,}],
+    }],
+    ({domCache, strokeWidth}: X) => [{
+        [refs.diamondBorderPart]: [{style:{strokeWidth:strokeWidth}}],
+    }],
+    ({domCache, innerHeight, innerWidth, innerX, innerY}: X) => [{
+        [refs.innerPart]: [,,{width: innerWidth, height: innerHeight, x: innerX, y: innerY}],
+    }],
+    xp.createShadow,
+] as PropAction[];
+const baseProp: PropDef = {
+    dry: true,
+    async: true,
+};
+const numProp: PropDef = {
+    ...baseProp,
+    type: Number,
+}
+const strProp: PropDef = {
+    ...baseProp,
+    type: String,
+}
+const propDefMap: PropDefMap<X> = {
+    ...xp.props,
+    width: numProp,
+    height: numProp, 
+    innerWidth: numProp, 
+    strokeWidth: numProp, 
+    innerHeight: numProp, 
+    innerX: numProp, 
+    innerY: numProp,
+    path: strProp,
+};
+const slicedPropDefs = xc.getSlicedPropDefs(propDefMap);
+xc.letThereBeProps(XtalFigDiamond, slicedPropDefs, 'onPropChange');
+xc.define(XtalFigDiamond);
