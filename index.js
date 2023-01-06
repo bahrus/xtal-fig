26ae24ff4251a0d9487407a4fad308747f12d6f7
Bruce Anderson <anderson.bruce.b@gmail.com>
2023-01-05 20:31:28 -0500

    0.0.22

--- index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-import('./xtal-fig-diamond.js');
-import('./xtal-fig-parallelogram.js');
-import('./xtal-fig-document.js');
-import('./xtal-fig-db-cylinder.js');
--- index.ts
+++
@@ -1,4 +0,0 @@
-import('./xtal-fig-diamond.js');
-import('./xtal-fig-parallelogram.js');
-import('./xtal-fig-document.js');
-import('./xtal-fig-db-cylinder.js');
